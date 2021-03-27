88f77bca47b6da246ddbd96f94dbd230b002b71d
AyresMonteiro <ayresmonteiro52@gmail.com>
2021-03-27 08:54:28 -0300

    [Add] @reach/router to App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Router } from '@reach/router';
+
+import LandingPage from './views/LandingPage';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <LandingPage path="/" />
+      </Router>
     </div>
   );
 }
---
+++ src/views/LandingPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function LandingPage () {
+  return (<>
+    <h1>Hello World React! (Again)</h1>
+  </>);
+}
+
+export default LandingPage;
